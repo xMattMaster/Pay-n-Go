a13bfed7df731d2c964f06d501073c32fb5dbce5
Matteo Arnese <68701124+xMattMaster@users.noreply.github.com>
2023-11-12 03:26:30 +0100

    Added top qualities widget to homepage
    Fixed errors and typos in various files. Added widgets to homepage explaining some of the features and services offered.

--- app/components/appbar.tsx
+++ app/components/appbar.tsx
@@ -5,22 +5,10 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import Typography from '@mui/material/Typography';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { getDesignTokens } from './../theme';
 
-
-const theme = createTheme({
-    palette: {
-        mode: 'light',
-        primary: {
-          main: 'rgb(15, 53, 156)',
-        },
-        secondary: {
-          main: 'rgb(245, 153, 22)',
-        },
-    },
-});
 
 export default function Component() {
     const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
@@ -31,7 +19,7 @@ export default function Component() {
         <ThemeProvider theme={getDesignTokens(prefersDarkMode ? 'dark' : 'light')}>
             <CssBaseline />
             <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                <Button size="small">Da aggiungere</Button>
+                <Button size="small">{'Da aggiungere'}</Button>
                 <Typography
                     component="h5"
                     variant="h5"
@@ -45,7 +33,7 @@ export default function Component() {
                     <SearchIcon />
                 </IconButton>
                 <Button variant="outlined" size="small">
-                    Accedi
+                    {'Accedi'}
                 </Button>
             </Toolbar>
         </ThemeProvider>
