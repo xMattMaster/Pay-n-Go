8d08ea0582c1f1c1caeea19ffe72099b582ad0bd
Matteo Arnese <68701124+xMattMaster@users.noreply.github.com>
2023-11-27 02:02:06 +0100

    Fixing horrible indentation #2 (why, Github??)

--- public/scripts/registration.php
+++ public/scripts/registration.php
@@ -1,46 +1,46 @@
 <?php
-	header('Access-Control-Allow-Origin: *');
-	header('Access-Control-Allow-Methods: POST');
-	header('Access-Control-Allow-Headers: Content-Type');
-	header('Content-Type: application/json');
-	$servername = "localhost";
-	$username = "basidati";
-	$password = "";
-	$dbname = "my_basidati";
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Content-Type');
+    header('Content-Type: application/json');
+    $servername = "localhost";
+    $username = "basidati";
+    $password = "";
+    $dbname = "my_basidati";
 
-	$content = trim(file_get_contents("php://input"));
-	$decoded = json_decode($content, true);
+    $content = trim(file_get_contents("php://input"));
+    $decoded = json_decode($content, true);
 
-	$email = $decoded['email'];
-	$pass = $decoded['password'];
-	$firstName = $decoded['firstName'];
-	$lastName = $decoded['lastName'];
-	$cfId = $decoded['cfId'];
-	$dateOfBirth = $decoded['dateOfBirth'];
-	$address = $decoded['address'];
+    $email = $decoded['email'];
+    $pass = $decoded['password'];
+    $firstName = $decoded['firstName'];
+    $lastName = $decoded['lastName'];
+    $cfId = $decoded['cfId'];
+    $dateOfBirth = $decoded['dateOfBirth'];
+    $address = $decoded['address'];
 
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-	// Check connection
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-	$sql1 = "INSERT INTO my_basidati.CLIENTI (Nome,Cognome,DataNascita,CodiceFiscale,Indirizzo) VALUES ('$firstName', '$lastName', '$dateOfBirth', '$cfId', '$address')";
-	$sql2 = "INSERT INTO my_basidati.UTENTI (Email,Password,Cliente) VALUES ('$email', '$pass', '$cfId')";
-	$res1 = $conn->query($sql1);
-	$res2 = $conn->query($sql2);
+    $sql1 = "INSERT INTO my_basidati.CLIENTI (Nome,Cognome,DataNascita,CodiceFiscale,Indirizzo) VALUES ('$firstName', '$lastName', '$dateOfBirth', '$cfId', '$address')";
+    $sql2 = "INSERT INTO my_basidati.UTENTI (Email,Password,Cliente) VALUES ('$email', '$pass', '$cfId')";
+    $res1 = $conn->query($sql1);
+    $res2 = $conn->query($sql2);
 
-	$output->res = 0;
-	//$output->email_got = $decoded['email'];  
-	//$output->password_got = $decoded['password'];
+    $output->res = 0;
+    //$output->email_got = $decoded['email'];  
+    //$output->password_got = $decoded['password'];
 
-	if ($res1 === TRUE && $res2 === TRUE) {
-		$output->res = 1;
-		} else {
-		$output->res = -1;
-		}
-		
-	echo JSON_encode($output);
+    if ($res1 === TRUE && $res2 === TRUE) {
+        $output->res = 1;
+    } else {
+        $output->res = -1;
+    }
+        
+    echo JSON_encode($output);
 ?>
