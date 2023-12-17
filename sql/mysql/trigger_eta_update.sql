3db2450aef6b2881f1fe9e51ff5f8b610d9d7134
Lorenzo Cecchini <z170s.lol@gmail.com>
2023-12-17 19:43:34 +0100

    Aggiunto trigger_eta che reagisce anche all'UPDATE, come specificato dal commit precedente

---
+++ sql/mysql/trigger_eta_update.sql
@@ -0,0 +1,19 @@
+-- Trigger che verifica se l'utente è maggiorenne
+DROP TRIGGER IF EXISTS verifica_eta_update;
+DELIMITER $$
+CREATE TRIGGER verifica_eta_update
+BEFORE UPDATE ON CLIENTI
+FOR EACH ROW
+BEGIN
+    DECLARE v_eta INT;
+
+    -- Calcola l'età sottraendo la data di nascita dalla data corrente
+    SET v_eta = TIMESTAMPDIFF(MONTH, NEW.DataNascita, NOW()) / 12;
+
+    -- Verifica se l'età è inferiore a 18 e genera un errore se necessario
+    IF v_eta < 18 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Devi essere maggiorenne per poterti registrare';
+    END IF;
+END $$
+DELIMITER ;
