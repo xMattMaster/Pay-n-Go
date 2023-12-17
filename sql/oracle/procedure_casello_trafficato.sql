db87b72dee30c09edf668e93368dd21c54eda9ef
Lorenzo Cecchini <z170s.lol@gmail.com>
2023-12-17 10:43:25 +0100

    Fixati alcuni errori negli script SQL. Si devono ancora tradurre in MySQL!!

--- sql/oracle/procedure_casello_trafficato.sql
+++ sql/oracle/procedure_casello_trafficato.sql
@@ -3,11 +3,8 @@ CREATE OR REPLACE PROCEDURE casello_trafficato(
     Out_max_id OUT CASELLI.Id%TYPE
 ) AS 
 BEGIN
-DECLARE
-    somma NUMBER(4) := 0;
-    max_id CASELLI.Id%TYPE;
 BEGIN
-    SELECT C.Id, COUNT(*) AS NUMERO_VOLTE INTO max_id, somma FROM (TRAGITTI T JOIN CASELLI C ON T.Casello_Ingresso = C.Id) JOIN CASELLI C2 ON C2.Id = T.Casello_Uscita
+    SELECT C.Id, COUNT(*) AS NUMERO_VOLTE INTO Out_max_id, Out_somma FROM (TRAGITTI T JOIN CASELLI C ON T.Casello_Ingresso = C.Id) JOIN CASELLI C2 ON C2.Id = T.Casello_Uscita
     GROUP BY C.Id
     ORDER BY NUMERO_VOLTE DESC
     FETCH FIRST 1 ROW ONLY;
