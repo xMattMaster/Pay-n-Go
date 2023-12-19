6316c4ce05c1024937bf4aba1511532bc594b3c0
Matteo Arnese <68701124+xMattMaster@users.noreply.github.com>
2023-12-19 01:09:19 +0100

    Update dashboard_payments_modify.php

--- public/scripts/dashboard_payments_modify.php
+++ public/scripts/dashboard_payments_modify.php
@@ -15,7 +15,7 @@
     $method = $decoded['method'];
     $code = $decoded['code'];
 
-    mysqli_report(MYSQLI_REPORT_STRICT | MYSQLI_REPORT_ALL);
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
     $output = new \stdClass();
     $output->res = 0;
